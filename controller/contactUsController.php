6d9f38e30fdcd24cb6a0f9c2c00639367184db4c
Lowik BOURGEOISAT <lowik.bourgeoisat@geneanet.org>
2020-06-25 20:42:58 +0200

    Add contact us page

---
+++ controller/contactUsController.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once( 'model/contactus.php' );
+
+/***************************
+----- LOAD CONTACTUS PAGE -----
+ ***************************/
+
+function contactUsPage() {
+
+    require('view/contactUsView.php');
+}
